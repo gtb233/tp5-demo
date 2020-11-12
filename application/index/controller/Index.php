671591646fddd8721499e5379ec8a4b9e927de62
gtb <gdluck256@gmail.com>
2020-11-13 00:07:34 +0800

    请求

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -24,17 +24,59 @@ class Index extends Controller
         return $this->fetch();
     }
 
-    public function req(Request $req)
+    public function req(Request $request)
     {
+        echo '模块：'.$request->module();
+        echo '<br/>控制器：'.$request->controller();
+        echo '<br/>操作：'.$request->action();
+        echo '<br/>';
+
         // 获取当前URL地址 不含域名
         echo 'url: ' . $this->request->url() . '<br/>';
         echo 'url: ' . \request()->url() . '<br/>';
-        echo 'url: ' . $req->url() . '<br/>';
+        echo 'url: ' . $request->url() . '<br/>';
 
         // 取参数，input()
-        dump($req->param());
-        echo 'name:'.$req->param('name');
-        echo 'name:'. input('name/s', 'gt233');
+        dump($request->param('name/s'));
+        echo 'name:'. input('name/s', 'gt233'). "<br/>";
+
+        echo '请求方法：' . $request->method() . '<br/>';
+        echo '资源类型：' . $request->type() . '<br/>';
+        echo '访问IP：' . $request->ip() . '<br/>';
+        echo '是否AJax请求：' . var_export($request->isAjax(), true) . '<br/>';
+        echo '请求参数：';
+        dump($request->param());
+        echo '请求参数：仅包含name';
+        dump($request->only(['name']));
+        echo '请求参数：排除name';
+        dump($request->except(['name']));
+
+        // 获取当前域名
+        echo 'domain: ' . $request->domain() . '<br/>';
+        // 获取当前入口文件
+        echo 'file: ' . $request->baseFile() . '<br/>';
+        // 获取当前URL地址 不含域名
+        echo 'url: ' . $request->url() . '<br/>';
+        // 获取包含域名的完整URL地址
+        echo 'url with domain: ' . $request->url(true) . '<br/>';
+        // 获取当前URL地址 不含QUERY_STRING
+        echo 'url without query: ' . $request->baseUrl() . '<br/>';
+        // 获取URL访问的ROOT地址
+        echo 'root:' . $request->root() . '<br/>';
+        // 获取URL访问的ROOT地址
+        echo 'root with domain: ' . $request->root(true) . '<br/>';
+        // 获取URL地址中的PATH_INFO信息
+        echo 'pathinfo: ' . $request->pathinfo() . '<br/>';
+        // 获取URL地址中的PATH_INFO信息 不含后缀
+        echo 'pathinfo: ' . $request->path() . '<br/>';
+        // 获取URL地址中的后缀信息
+        echo 'ext: ' . $request->ext() . '<br/>';
+
+
+        echo '路由信息：';
+        dump($request->routeInfo());
+        echo '调度信息：';
+        dump($request->dispatch());
 
         return '';
     }
