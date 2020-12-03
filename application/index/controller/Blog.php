c1795659c0cf0ea32df60e07b55bf881ea602a72
gtb <gdluck256@gmail.com>
2020-12-03 19:18:56 +0800

    PHP8测试

--- application/index/controller/Blog.php
+++ application/index/controller/Blog.php
@@ -37,4 +37,32 @@ class Blog
 
         return '123';
     }
+
+    public function baseTest()
+    {
+        $obj = new \stdClass();
+        if ("0") echo '0'       ;  echo PHP_EOL.'<br/>';
+        if (0.0) echo '0.0'     ;  echo PHP_EOL.'<br/>';
+        if ("0.0") echo 'STR: 0.0'  ; echo PHP_EOL.'<br/>';
+        if (null) echo 'null'   ;  echo PHP_EOL.'<br/>';
+        if ([]) echo '[]'       ;  echo PHP_EOL.'<br/>';
+        if ([0]) echo '[0]'     ;  echo PHP_EOL.'<br/>';
+        if ($obj) echo 'stdClass'; echo PHP_EOL.'<br/>';
+        echo 'empty'; echo PHP_EOL.'<br/>';
+
+        echo PHP_EOL.'<br/>';
+
+        // PHP8 变更为 数字与字符比较非严格下转成字符串比较
+        echo 0 == "0" ? 1 : 0  ; echo PHP_EOL.'<br/>';
+        echo 0 == "0.0" ? 1 : 0; echo PHP_EOL.'<br/>';
+        echo 0 == "foo" ? 1 : 0; echo PHP_EOL.'<br/>';
+        echo 0 == ""    ? 1 : 0; echo PHP_EOL.'<br/>';
+        echo 42 == " 42"    ? 1 : 0; echo PHP_EOL.'<br/>';
+        echo 42 == "42foo"    ? 1 : 0; echo PHP_EOL.'<br/>';
+
+        $a = $b = 1;
+        echo "sum:" . ($a + $b); echo PHP_EOL.'<br/>';
+
+
+    }
 }
